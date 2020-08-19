6b1034fbc4f61ed9d673208f8b51d51af2f5f1f6
Segun Famisa <segun.famisa@soundcloud.com>
2020-08-19 16:50:57 +0200

    Configure ktlint

--- src/com/segunfamisa/Application.kt
+++ src/com/segunfamisa/Application.kt
@@ -1,10 +1,12 @@
 package com.segunfamisa
 
-import io.ktor.application.*
-import io.ktor.http.*
-import io.ktor.response.*
-import io.ktor.routing.*
-import io.ktor.server.netty.*
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.netty.EngineMain
 
 fun main(args: Array<String>): Unit = EngineMain.main(args)
 
